b4ac167631cc4277112f5e5ef770e78570b9b446
Anatoliy Pushkarev <anatoliypuss5@gmail.com>
2024-04-23 22:05:58 +0300

    Code formatting and modified pipeline, added Scoop

--- scripts/build_projectdb.py
+++ scripts/build_projectdb.py
@@ -1,40 +1,49 @@
-import psycopg2 as psql
-from pprint import pprint
+"""
+    This module implements methods,
+    which works with PostgreSQL database.
+    It creates schema of tables 'city' and
+    'database', imports data there and tests
+    the database by selecting first rows.
+"""
+
 import os
+from pprint import pprint
+import psycopg2 as psql
 
 
 # Read password from secrets file
 file = os.path.join("secrets", ".psql.pass")
 
-with open(file, "r") as file:
-        password=file.read().rstrip()
+with open(file, "r", encoding='utf-8') as file:
+    password = file.read().rstrip()
 
 # build connection string
-conn_string="host=hadoop-04.uni.innopolis.ru port=5432 user=team29 dbname=team29_projectdb password={}".format(password)
+CONN_STRING = f"host=hadoop-04.uni.innopolis.ru port=5432 user=team29 \
+                dbname=team29_projectdb password={password}"
 
 
 # Connect to the remote dbms
-with psql.connect(conn_string) as conn:
-        
-        # Create a cursor for executing psql commands
-        cur = conn.cursor()
-        # Read the commands from the file and execute them.
+with psql.connect(CONN_STRING) as conn:
 
-        print('Creating tables...')
-        with open(os.path.join("sql","create_tables.sql")) as file:
-                content = file.read()
-                cur.execute(content)
-        conn.commit()
+    # Create a cursor for executing psql commands
+    cur = conn.cursor()
+    # Read the commands from the file and execute them.
 
-        # Read the commands from the file and execute them.
-        print('Importing data...')
-        with open(os.path.join("sql", "import_data.sql")) as file:
-                # We assume that the COPY commands in the file are ordered (1.city, 2.dataset)
-                commands= file.readlines()
-                with open(os.path.join("data","city.csv"), "r") as depts:
-                        cur.copy_expert(commands[0], depts)
-                with open(os.path.join("data","dataset.csv"), "r") as emps:
-                        cur.copy_expert(commands[1], emps)
+    print('Creating tables...')
+    with open(os.path.join("sql", "create_tables.sql"), encoding='utf-8') as file:
+        content = file.read()
+        cur.execute(content)
+    conn.commit()
+
+    # Read the commands from the file and execute them.
+    print('Importing data...')
+    with open(os.path.join("sql", "import_data.sql"), encoding='utf-8') as file:
+        # We assume that the COPY commands in the file are ordered (1.city, 2.dataset)
+        commands = file.readlines()
+        with open(os.path.join("data", "city.csv"), "r", encoding='utf-8') as depts:
+            cur.copy_expert(commands[0], depts)
+        with open(os.path.join("data", "dataset.csv"), "r", encoding='utf-8') as emps:
+            cur.copy_expert(commands[1], emps)
 
         # If the sql statements are CRUD then you need to commit the change
         conn.commit()
@@ -43,9 +52,9 @@ with psql.connect(conn_string) as conn:
         cur = conn.cursor()
         # Read the sql commands from the file
         print('Testing database...')
-        with open(os.path.join("sql", "test_database.sql")) as file:
-                commands = file.readlines()
-                for command in commands:
-                        cur.execute(command)
-                        # Read all records and print them                        
-                        pprint(cur.fetchall())
+        with open(os.path.join("sql", "test_database.sql"), encoding='utf-8') as file:
+            commands = file.readlines()
+            for command in commands:
+                cur.execute(command)
+                # Read all records and print them
+                pprint(cur.fetchall())
--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,6 +1,14 @@
-import pandas as pd
+"""
+    This module implements preprocessing
+    of the initial dataset by combining
+    all csv`s together, adding null values
+    and creating a separate city file
+    for city id mapping.
+"""
+
 import os
 import glob
+import pandas as pd
 
 data_folder = os.path.join('data')
 outdir = os.path.join(data_folder, 'preprocessed')
@@ -9,7 +17,7 @@ if not os.path.exists(outdir):
 
 # creating mapping of city to city_id
 
-mapping = dict()
+mapping = {}
 
 csv_files = glob.glob(os.path.join(data_folder, '*.csv'))
 for ind, file in enumerate(csv_files):
@@ -21,13 +29,21 @@ values = []
 for city, city_id in mapping.items():
     values.append([city_id, city])
 city = pd.DataFrame(values, columns=['city_id', 'city_name'])
-city.to_csv(os.path.join(outdir, 'city.csv'), sep=',', header=True, index=False)
+city.to_csv(os.path.join(outdir, 'city.csv'),
+            sep=',', header=True, index=False)
 
 # combining all csv together
 
-def update_csv(city_name, city_id):
-    df = pd.read_csv(os.path.join(data_folder, city_name + ".csv"))
-    df.columns = [
+
+def update_csv(update_city_name, update_city_id):
+    """
+        This function preprocess a single csv of a city
+        by renaming columns to sql-like type,
+        adding corresponding city_id and null values
+        of missing measurements.
+    """
+    city_df = pd.read_csv(os.path.join(data_folder, update_city_name + ".csv"))
+    city_df.columns = [
         'date_time',
         'max_temp',
         'min_temp',
@@ -55,23 +71,25 @@ def update_csv(city_name, city_id):
         'wind_speed'
     ]
 
-    def check(v):
-        if ('no' in v.lower()):
+    def check(col_value):
+        if 'no' in col_value.lower():
             return 'NULL'
-        return v
-    
+        return col_value
+
     columns = ['moonset', 'moonrise']
     for col in columns:
-        df[col] = df[col].apply(check)
+        city_df[col] = city_df[col].apply(check)
 
-    df['city_id'] = city_id
-    return df
+    city_df['city_id'] = update_city_id
+    return city_df
 
-updated_csv = [] 
+
+updated_csv = []
 for city, city_id in mapping.items():
     updated_csv.append(update_csv(city, city_id))
 
 combined = pd.concat(updated_csv, axis=0)
-combined.to_csv(os.path.join(outdir, 'dataset.csv'), sep=',', header=True, index=False)
+combined.to_csv(os.path.join(outdir, 'dataset.csv'),
+                sep=',', header=True, index=False)
 
 print('Done preprocessing')
