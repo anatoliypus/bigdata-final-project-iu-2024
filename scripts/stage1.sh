01bbcf641b225b45ab6c99c620f0136e2473541c
Anatoliy Pushkarev <anatoliypuss5@gmail.com>
2024-04-23 22:05:58 +0300

    Code formatting and modified pipeline, added Scoop

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 python3 scripts/build_projectdb.py
+
+rm -rf /home/team29/project/warehouse
+mkdir -p /home/team29/project/warehouse
+
+password=$(head -n 1 secrets/.psql.pass)
+sqoop import-all-tables --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team29_projectdb --username team29 --password $password --compression-codec=snappy --compress --as-avrodatafile --warehouse-dir=/home/team29/project/warehouse --m 1
+
+rm -rf output/*.java
+rm -rf output/*.avsc
+
+mv *.java output/
+mv *.avsc output/
