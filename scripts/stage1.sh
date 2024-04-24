b599e86d8345faa7aa4b9c77ad08042ddee44bb0
Anatoliy Pushkarev <anatoliypuss5@gmail.com>
2024-04-24 11:59:58 +0300

    Fix in path and config

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -2,11 +2,9 @@
 
 python3 scripts/build_projectdb.py
 
-rm -rf /home/team29/project/warehouse
-mkdir -p /home/team29/project/warehouse
-
 password=$(head -n 1 secrets/.psql.pass)
-sqoop import-all-tables --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team29_projectdb --username team29 --password $password --compression-codec=snappy --compress --as-avrodatafile --warehouse-dir=/home/team29/project/warehouse --m 1
+hdfs dfs -rm -r -f /user/team29/project/warehouse
+sqoop import-all-tables --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team29_projectdb --username team29 --password $password --compression-codec=snappy --compress --as-avrodatafile --warehouse-dir=/user/team29/project/warehouse --m 1
 
 rm -rf output/*.java
 rm -rf output/*.avsc
